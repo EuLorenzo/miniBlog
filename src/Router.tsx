6aa6b3a5065683804d31fad9410e4c335002ce0a
euLorenzo <lorenzo.frinhani@gmail.com>
2025-03-07 15:37:10 -0300

    Front do register feito

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const Router = () => {
   return (
@@ -9,6 +11,8 @@ const Router = () => {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   );
